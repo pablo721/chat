3945a78cd5d73824d131b05e2520af5c2b2c6c5a
pablo <pablo774@protonmail.com>
2023-02-09 01:53:59 +0100

    x

--- chatapp/settings_local.py
+++ chatapp/settings_local.py
@@ -128,6 +128,8 @@ USE_TZ = True
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
