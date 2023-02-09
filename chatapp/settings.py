d3be9368144393a827bead5535c5ce53e7826fa2
pablo <pablo774@protonmail.com>
2023-02-09 01:55:25 +0100

    x

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -142,7 +142,7 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'staticfiles')
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
