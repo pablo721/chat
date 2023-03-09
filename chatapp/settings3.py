a6b9e9389e60b6ac44ac51edc67a0552f8d93f29
pablo <pablo774@protonmail.com>
2023-03-09 11:50:46 +0100

    rework

---
+++ chatapp/settings3.py
@@ -0,0 +1,151 @@
+# import os
+# from pathlib import Path
+# import dj_database_url
+#
+# # Build paths inside the project like this: BASE_DIR / 'subdir'.
+# BASE_DIR = Path(__file__).resolve().parent.parent
+#
+#
+# # Quick-start development settings - unsuitable for production
+# # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+#
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = os.environ.get('DJANGO_SECRET')
+#
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
+#
+# ALLOWED_HOSTS = [
+#     'localhost',
+#     '127.0.0.1',
+#     'chat90.herokuapp.com'
+# ]
+#
+#
+# # Application definition
+#
+# INSTALLED_APPS = [
+#     'django.contrib.admin',
+#     'django.contrib.auth',
+#     'django.contrib.contenttypes',
+#     'django.contrib.sessions',
+#     'django.contrib.messages',
+#     'django.contrib.staticfiles',
+#     'whitenoise.runserver_nostatic',
+#     'rest_framework',
+#     'online_users',
+#     'chat',
+#     'website',
+#     'api',
+# ]
+#
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#
+# MIDDLEWARE = [
+#     'whitenoise.middleware.WhiteNoiseMiddleware',
+#     'django.middleware.security.SecurityMiddleware',
+#     'django.contrib.sessions.middleware.SessionMiddleware',
+#     'django.middleware.common.CommonMiddleware',
+#     'django.middleware.csrf.CsrfViewMiddleware',
+#     'django.contrib.auth.middleware.AuthenticationMiddleware',
+#     'django.contrib.messages.middleware.MessageMiddleware',
+#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+#     'online_users.middleware.OnlineNowMiddleware',
+# ]
+#
+# ROOT_URLCONF = 'chatapp.urls'
+#
+# TEMPLATES = [
+#     {
+#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+#         'DIRS': [],
+#         'APP_DIRS': True,
+#         'OPTIONS': {
+#             'builtins': ['django.templatetags.static'],
+#             'context_processors': [
+#                 'django.template.context_processors.debug',
+#                 'django.template.context_processors.request',
+#                 'django.contrib.auth.context_processors.auth',
+#                 'django.contrib.messages.context_processors.messages',
+#             ],
+#         },
+#     },
+# ]
+#
+# WSGI_APPLICATION = 'chatapp.wsgi.application'
+#
+#
+# # Database
+# # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+#
+#
+#
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': os.environ.get('HEROKU_DB'),
+#         'USER': os.environ.get('HEROKU_USER'),
+#         'PASSWORD': os.environ.get('HEROKU_PASS'),
+#         'HOST': os.environ.get('HEROKU_HOST'),
+#         'POST': '5432'
+#     },
+#     'default2': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'chatdb',
+#         'USER': 'msg',
+#         'PASSWORD': os.environ.get('LOCAL_DB_PASS'),
+#         'HOST': 'localhost',
+#         'POST': '5432'
+#     }
+# }
+#
+#
+# # Password validation
+# # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
+#
+# AUTH_PASSWORD_VALIDATORS = [
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+#     },
+# ]
+#
+#
+# # Internationalization
+# # https://docs.djangoproject.com/en/4.0/topics/i18n/
+#
+# LANGUAGE_CODE = 'en-us'
+#
+# TIME_ZONE = 'UTC'
+#
+# USE_I18N = True
+#
+# USE_TZ = True
+#
+#
+# # Static files (CSS, JavaScript, Images)
+# # https://docs.djangoproject.com/en/4.0/howto/static-files/
+#
+# MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+# MEDIA_URL = '/media/'
+#
+#
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'staticfiles')
+# ]
+#
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# CSRF_TRUSTED_ORIGINS = ['https://chat90.herokuapp.com']
+#
+#
